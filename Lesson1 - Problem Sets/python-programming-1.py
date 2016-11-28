eb6d4c371b671ca86dd139b0b6d86906a8ced86e
Rabia Chaudry <rabia.chaudry@nicta.com.au>
2016-11-28 21:15:25 +1100

    Lesson1 - Python Programming 1: number of days in 7 weeks

---
+++ Lesson1 - Problem Sets/python-programming-1.py
@@ -0,0 +1,8 @@
+# Write Python code that prints out the number of hours in 7 weeks.
+weeks = 7
+days_per_week = 7
+hours_per_day = 24
+
+hours = weeks * days_per_week * hours_per_day
+
+print hours 
