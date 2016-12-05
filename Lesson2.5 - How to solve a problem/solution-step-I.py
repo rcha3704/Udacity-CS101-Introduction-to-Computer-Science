476d4e4f1b4653cc5cda86be5533674fee7c5274
Rabia Chaudry <rabia.chaudry@nicta.com.au>
2016-12-05 11:36:22 +1100

    Solution Step 1

---
+++ Lesson2.5 - How to solve a problem/solution-step-I.py
@@ -0,0 +1,51 @@
+# Credit goes to Websten from forums
+#
+# Use Dave's suggestions to finish your daysBetweenDates
+# procedure. It will need to take into account leap years
+# in addition to the correct number of days in each month.
+
+def daysInMonth(year,month):
+    return 30
+
+def nextDay(year, month, day):
+    """Simple version: assume every month has 30 days"""
+    if day < daysInMonth(year,month):
+        return year, month, day + 1
+    else:
+        if month == 12:
+            return year + 1, 1, 1
+        else:
+            return year, month + 1, 1
+        
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    """Returns True if year1-month1-day1 is before year2-month2-day2. Otherwise, returns False."""
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False        
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar."""
+    # program defensively! Add an assertion if the input is not valid!
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        days += 1
+    return days
+
+def myTests():
+    assert daysBetweenDates(2013,1,1,2013,1,1) == 0
+    assert daysBetweenDates(2013,1,1,2013,1,2) == 1
+    assert nextDay(2013,1,1) == (2013,1,2)
+    assert nextDay(2013,4,30) == (2013,5,1)
+    assert nextDay(2012,12,31) == (2013,1,1)
+    print "Tests finished!!!"
+    
+myTests()
