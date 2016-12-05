1a27d665f4390272f5bf734bb043b9e905b7f4ef
Rabia Chaudry <rabia.chaudry@nicta.com.au>
2016-12-05 23:48:03 +1100

    Quiz: Greatest

---
+++ Lesson3 - Problem Sets/greatest.py
@@ -0,0 +1,19 @@
+# Define a procedure, greatest,
+# that takes as input a list
+# of positive numbers, and
+# returns the greatest number
+# in that list. If the input
+# list is empty, the output
+# should be 0.
+
+def greatest(list_of_numbers):
+    max = 0
+    for n in list_of_numbers:
+        if n > max:
+            max = n
+    return max
+
+print greatest([4,23,1])
+#>>> 23
+print greatest([])
+#>>> 0
