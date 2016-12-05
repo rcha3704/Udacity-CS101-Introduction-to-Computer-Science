00639527eba67741cd7872a7d2f0226ccc06cc80
Rabia Chaudry <rabia.chaudry@nicta.com.au>
2016-12-06 00:09:11 +1100

    Quiz: Symmetric Square

---
+++ Lesson3 - Problem Sets/symmetric-square.py
@@ -0,0 +1,48 @@
+# A list is symmetric if the first row is the same as the first column,
+# the second row is the same as the second column and so on. Write a
+# procedure, symmetric, which takes a list as input, and returns the
+# boolean True if the list is symmetric and False if it is not.
+def symmetric(lst):
+    n = len(lst)
+    if n>0:
+        n1 = len(lst[0])
+        if n != n1:
+            return False
+            
+    i = 0
+    while i<n:
+        j = 0
+        while j<n:
+            if lst[i][j] != lst[j][i]:
+                return False
+            j+=1
+        i+=1
+    return True
+
+print symmetric([[1, 2, 3],
+                [2, 3, 4],
+                [3, 4, 1]])
+#>>> True
+
+print symmetric([["cat", "dog", "fish"],
+                ["dog", "dog", "fish"],
+                ["fish", "fish", "cat"]])
+#>>> True
+
+print symmetric([["cat", "dog", "fish"],
+                ["dog", "dog", "dog"],
+                ["fish","fish","cat"]])
+#>>> False
+
+print symmetric([[1, 2],
+                [2, 1]])
+#>>> True
+
+print symmetric([[1, 2, 3, 4],
+                [2, 3, 4, 5],
+                [3, 4, 5, 6]])
+#>>> False
+
+print symmetric([[1,2,3],
+                [2,3,1]])
+#>>> False
