0681cb9d2c66208f479d79f2277e38b1f14aae9d
Rabia Chaudry <rabia.chaudry@nicta.com.au>
2016-12-11 10:55:38 +1100

    Quiz: Shift a letter

---
+++ Lesson5 - Problem Sets/shift-a-letter.py
@@ -0,0 +1,14 @@
+# Write a procedure, shift, which takes as its input a lowercase letter,
+# a-z and returns the next letter in the alphabet after it, with 'a' 
+# following 'z'.
+
+def shift(letter):
+    return chr(97+(((ord(letter)%97)+1)%26))
+
+
+print shift('a')
+#>>> b
+print shift('n')
+#>>> o
+print shift('z')
+#>>> a
